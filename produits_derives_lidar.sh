0594c615ba0705fb9f065dbf1126e47d54d0179c
mdupays <malvina.dupays@ign.fr>
2022-11-15 17:08:31 +0100

    add:Update .sh for lauching the script: new data

--- produits_derives_lidar.sh
+++ produits_derives_lidar.sh
@@ -4,10 +4,10 @@ conda env create -n dtm -f environment.yml
 conda activate dtm
 
 # Step 1 : filter ground
-python3 gf_main.py /var/data/store-lidarhd/projet-LHD/Malvina/poc/data/ /home/MDupays/ laz
+python3 gf_main.py /var/data/store-lidarhd/projet-LHD/Malvina/POC/MQ/data/ /var/data/store-lidarhd/projet-LHD/Malvina/POC/MQ/ laz
 # Step 2 ; create DTM
-python3 ip_main.py /var/data/store-lidarhd/projet-LHD/Malvina/poc/ /home/MDupays/ laz 0 0.5
-python3 ip_main.py /var/data/store-lidarhd/projet-LHD/Malvina/poc/ /home/MDupays/ laz 0 0.5 startin-TINlinear
-python3 ip_main.py /var/data/store-lidarhd/projet-LHD/Malvina/poc/ /home/MDupays/ laz 0 0.5 PDAL-IDW
-python3 ip_main.py /var/data/store-lidarhd/projet-LHD/Malvina/poc/ /home/MDupays/ laz 0 0.5 CGAL-NN
-python3 ip_main.py /var/data/store-lidarhd/projet-LHD/Malvina/poc/ /home/MDupays/ laz 0 0.5 IDWquad
+python3 ip_main.py /var/data/store-lidarhd/projet-LHD/Malvina/POC/MQ/data/ /var/data/store-lidarhd/projet-LHD/Malvina/POC/MQ/ laz 0 0.5
+python3 ip_main.py /var/data/store-lidarhd/projet-LHD/Malvina/POC/MQ/data/ /var/data/store-lidarhd/projet-LHD/Malvina/POC/MQ/ laz 0 0.5 startin-TINlinear
+python3 ip_main.py /var/data/store-lidarhd/projet-LHD/Malvina/POC/MQ/data/ /var/data/store-lidarhd/projet-LHD/Malvina/POC/MQ/ laz 0 0.5 PDAL-IDW
+python3 ip_main.py /var/data/store-lidarhd/projet-LHD/Malvina/POC/MQ/data/ /var/data/store-lidarhd/projet-LHD/Malvina/POC/MQ/ laz 0 0.5 CGAL-NN
+python3 ip_main.py /var/data/store-lidarhd/projet-LHD/Malvina/POC/MQ/data/ /var/data/store-lidarhd/projet-LHD/Malvina/POC/MQ/ laz 0 0.5 IDWquad
