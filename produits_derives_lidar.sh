a8066fc3a53b96061fe93a24a5b69316d8b8839a
mdupays <malvina.dupays@ign.fr>
2022-11-17 09:50:35 +0100

    update: script for lauching the severals steps

--- produits_derives_lidar.sh
+++ produits_derives_lidar.sh
@@ -5,7 +5,7 @@ conda activate dtm
 
 # Step 1 : filter ground
 python3 gf_main.py /var/data/store-lidarhd/projet-LHD/Malvina/POC/MQ/data/ /var/data/store-lidarhd/projet-LHD/Malvina/POC/MQ/ laz
-# Step 2 ; create DTM
+# Step 2 ; create DTM with the severals method
 python3 ip_main.py /var/data/store-lidarhd/projet-LHD/Malvina/POC/MQ/data/ /var/data/store-lidarhd/projet-LHD/Malvina/POC/MQ/ laz 0 0.5
 python3 ip_main.py /var/data/store-lidarhd/projet-LHD/Malvina/POC/MQ/data/ /var/data/store-lidarhd/projet-LHD/Malvina/POC/MQ/ laz 0 0.5 startin-TINlinear
 python3 ip_main.py /var/data/store-lidarhd/projet-LHD/Malvina/POC/MQ/data/ /var/data/store-lidarhd/projet-LHD/Malvina/POC/MQ/ laz 0 0.5 PDAL-IDW
