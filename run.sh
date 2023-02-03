334cf4eaa885e4ab18ee7ed155f149de335eb97d
mdupays <malvina.dupays@ign.fr>
2023-02-03 12:22:52 +0100

    update run.sh

--- run.sh
+++ run.sh
@@ -16,6 +16,12 @@ while getopts "h?i:o:f:" opt; do
       ;;
     i)  INPUT=${OPTARG}
       ;;
+      ;;
+    i_s)  INPUT_DSM=${OPTARG}
+      ;;
+      ;;
+    i_t)  INPUT_DTM=${OPTARG}
+      ;;
     o)  OUTPUT=${OPTARG}
       ;;
     f)  FORMAT=${OPTARG}
@@ -24,11 +30,23 @@ while getopts "h?i:o:f:" opt; do
 done
 
 # Step 1 : filter ground
-python -m produit_derive_lidar.gf_multiprocessing -i ${INPUT} -o ${OUTPUT}/DTM -t ${OUTPUT}/_tmp -e ${FORMAT}
+python -m produit_derive_lidar.gf_multiprocessing -i ${INPUT} -o ${OUTPUT}/DTM -t ${OUTPUT}/_tmp -e ${FORMAT} --keep_classes 2 9 66
 # Step 2 ; create DTM with the severals method
 python -m produit_derive_lidar.ip_multiprocessing -i ${INPUT} -o ${OUTPUT}/DTM -t ${OUTPUT}/_tmp -e ${FORMAT} -p 0 -s 0.5
 python -m produit_derive_lidar.ip_multiprocessing -i ${INPUT} -o ${OUTPUT}/DTM -t ${OUTPUT}/_tmp  -e ${FORMAT} -p 0 -s 0.5 -m startin-TINlinear
 python -m produit_derive_lidar.ip_multiprocessing -i ${INPUT} -o ${OUTPUT}/DTM -t ${OUTPUT}/_tmp -e ${FORMAT} -p 0 -s 0.5 -m PDAL-IDW
 python -m produit_derive_lidar.ip_multiprocessing -i ${INPUT} -o ${OUTPUT}/DTM -t ${OUTPUT}/_tmp  -e ${FORMAT} -p 0 -s 0.5 -m CGAL-NN
+# Step 3 : filter ground and upground 
+python -m produit_derive_lidar.gf_multiprocessing -i ${INPUT} -o ${OUTPUT}/DTM -t ${OUTPUT}/_tmp -e ${FORMAT} --keep_classes 2 3 4 5 6 9 66
+# Step 4 ; create DSM with the severals method
+python -m produit_derive_lidar.ip_multiprocessing -i ${INPUT} -o ${OUTPUT}/DTM -t ${OUTPUT}/_tmp -e ${FORMAT} -p 0 -s 0.5
+python -m produit_derive_lidar.ip_multiprocessing -i ${INPUT} -o ${OUTPUT}/DTM -t ${OUTPUT}/_tmp  -e ${FORMAT} -p 0 -s 0.5 -m startin-TINlinear
+python -m produit_derive_lidar.ip_multiprocessing -i ${INPUT} -o ${OUTPUT}/DTM -t ${OUTPUT}/_tmp -e ${FORMAT} -p 0 -s 0.5 -m PDAL-IDW
+python -m produit_derive_lidar.ip_multiprocessing -i ${INPUT} -o ${OUTPUT}/DTM -t ${OUTPUT}/_tmp  -e ${FORMAT} -p 0 -s 0.5 -m CGAL-NN
+# Step 4 ; create DHM with the severals method
+python -m produit_derive_lidar.dhm_multiprocessing -i ${INPUT} -i_m ${INPUT_DSM} -i_t ${INPUT_DTM} -o ${OUTPUT}/DTM -t ${OUTPUT}/_tmp -e ${FORMAT} -p 0 -s 0.5
+python -m produit_derive_lidar.dhm_multiprocessing -i ${INPUT} -i_m ${INPUT_DSM} -i_t ${INPUT_DTM} -o ${OUTPUT}/DTM -t ${OUTPUT}/_tmp  -e ${FORMAT} -p 0 -s 0.5 -m startin-TINlinear
+python -m produit_derive_lidar.dhm_multiprocessing -i ${INPUT} -i_m ${INPUT_DSM} -i_t ${INPUT_DTM} -o ${OUTPUT}/DTM -t ${OUTPUT}/_tmp -e ${FORMAT} -p 0 -s 0.5 -m PDAL-IDW
+python -m produit_derive_lidar.dhm_multiprocessing -i ${INPUT} -i_m ${INPUT_DSM} -i_t ${INPUT_DTM} -o ${OUTPUT}/DTM -t ${OUTPUT}/_tmp  -e ${FORMAT} -p 0 -s 0.5 -m CGAL-NN
 # Not working for now:
 # python -m produit_derive_lidar.ip_multiprocessing -i ${INPUT} -o ${OUTPUT}/DTM -t ${OUTPUT}/_tmp  -e ${FORMAT} -p 0 -s 0.5 -m IDWquad
