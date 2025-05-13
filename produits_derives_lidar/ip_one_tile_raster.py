06005c47d843f8097fc9c375325b70bc5a1a2e03
mdupays <malvina.dupays@ign.fr>
2025-05-13 17:17:44 +0200

    add configs for extract stat from raster

--- produits_derives_lidar/ip_one_tile_raster.py
+++ produits_derives_lidar/ip_one_tile_raster.py
@@ -21,25 +21,31 @@ def run_ip_on_tile_raster(config: DictConfig):
     """Run extract z virtual lines on single tile using hydra config
     config parameters are explained in the default.yaml files
     """
-    tilename_raster, _ = os.path.splitext(config.io.input_raster_filename)
-    input_raster = os.path.join(config.io.input_raster_dir, f"{tilename_raster}.tif")  # path to the RASTER file
+    tilename_raster, _ = os.path.splitext(config.extract_stat.input_raster_filename)
+    input_raster = os.path.join(
+        config.extract_stat.input_raster_dir, f"{tilename_raster}.tif"
+    )  # path to the RASTER file
     if not os.path.isfile(input_raster):
         raise ValueError(f"Input raster file not found: {input_raster}")
 
-    tilename_geom, _ = os.path.splitext(config.io.input_geometry_filename)
-    input_geometry = os.path.join(config.io.input_geometry_dir, f"{tilename_geom}.shp")  # path to the geometry file
+    tilename_geom, _ = os.path.splitext(config.extract_stat.input_geometry_filename)
+    input_geometry = os.path.join(
+        config.extract_stat.input_geometry_dir, f"{tilename_geom}.shp"
+    )  # path to the geometry file
     if not os.path.isfile(input_geometry):
         raise ValueError(f"Input gemetry file not found: {input_geometry}")
 
-    output_dir = config.io.output_dir
+    output_dir = config.extract_stat.output_dir
     if output_dir is None:
-        raise ValueError("""config.io.output_dir is empty, please provide an input directory in the configuration""")
-    os.makedirs(config.io.output_dir, exist_ok=True)
+        raise ValueError(
+            """config.extract_stat.output_dir is empty, please provide an input directory in the configuration"""
+        )
+    os.makedirs(config.extract_stat.output_dir, exist_ok=True)
 
     # for export
-    crs = config.io.spatial_reference
+    crs = config.extract_stat.spatial_reference
     geometry_filename = f"{tilename_raster}_lines.GeoJSON"
-    output_geometry = os.path.join(config.io.output_dir, geometry_filename)
+    output_geometry = os.path.join(config.extract_stat.output_dir, geometry_filename)
 
     extract_z_virtual_lines_from_raster(input_geometry, input_raster, output_geometry, crs)
 
